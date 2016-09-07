a7c2d7f3149791b34ba50d0ad48a6a1746362b25
Architha Ellappalayam <archita.e@gmail.com>
2016-09-07 11:44:42 +0200

    Uploading dataset GSE29008
    Here the datasets for the HCT8 cells from GEO dataset GSE29008 are uploaded. The data is read into hct_eight. RMA normalization is performed on the data followed by converting it into an expression matrix. The column names are changed according to the conditions in which they were examined. here the effects of the toxins were examined at 2,6 and 24 hours

--- compare_datasets.r
+++ compare_datasets.r
@@ -51,3 +51,18 @@ library(hgu133plus2probe)
 library(mouse4302.db)
 library(mouse4302cdf)
 library(mouse4302probe)
+
+#Read the data files
+hct_eight <- ReadAffy(cdfname = "hgu133plus2cdf" )
+
+#Processing the data in RMA menthod with background correction
+hct_eight_rma <- rma(hct_eight)
+
+#creating the expression matrix(genes in rows and condiitons in columns)
+hct_eight_normal = exprs(hct_eight_rma)
+
+#Setting the column names in the matrix
+colnames(hct_eight_normal) = c("Control_2h_R1","Control_2h_R2","ToxA_2h_R1","ToxA_2h_R2","ToxB_2h_R1","ToxB_2h_R2","Control_6h_R2","Control_6h_R3","ToxA_6h_R1","ToxA_6h_R2","ToxA_6h_R3","ToxB_6h_R1","ToxB_6h_R2","ToxB_6h_R3","Control_24h_R1","Control_24h_R3", "ToxA_24h_R1", "ToxA_24h_R3","ToxB_24h_R1", "Toxb_24h_R3")
+
+#log transforming the expression values for normal distribution
+hct_eight_normal = log(hct_eight_normal, 2)
